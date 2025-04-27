a49630f526ea46595a119cd6933b41ea36324c19
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2025-04-27 20:36:48 +0700

    remove gesture handling

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 local Blitbuffer = require("ffi/blitbuffer")
-local Device = require("device")
 local Geom = require("ui/geometry")
 local LineWidget = require("ui/widget/linewidget")
 local MovableContainer = require("ui/widget/container/movablecontainer")
@@ -7,7 +6,6 @@ local FrameContainer = require("ui/widget/container/framecontainer")
 local CenterContainer = require("ui/widget/container/centercontainer")
 local VerticalGroup = require("ui/widget/verticalgroup")
 local Screen = require("device").screen
-local GestureRange = require("ui/gesturerange")
 local UIManager = require("ui/uimanager")
 local InputContainer = require("ui/widget/container/inputcontainer")
 local _ = require("gettext")
@@ -25,16 +23,6 @@ local ReadingRuler = InputContainer:extend({
     _line_thickness = 5,
     _show_overlay = false,
     _movable = nil,
-    _ignore_events = {
-        -- handle these events ourselves (to call the movablecontainer)
-        "hold",
-        "hold_release",
-        "hold_pan",
-        "swipe",
-        "touch",
-        "pan",
-        "pan_release",
-    },
 })
 
 function ReadingRuler:init()
@@ -42,24 +30,6 @@ function ReadingRuler:init()
 
     self.ui.menu:registerToMainMenu(self)
     self.view:registerViewModule("reading_ruler", self)
-
-    if Device:isTouchDevice() then
-        logger.info("--- ReadingRuler touch ---")
-
-        -- Register a range that covers the whole screen
-        local range = Geom:new({ x = 0, y = 0, w = Screen:getWidth(), h = Screen:getHeight() })
-
-        -- Register gesture events to handle touches anywhere on screen
-        self.ges_events = {
-            Touch = { GestureRange:new({ ges = "tap", range = range }) },
-            Swipe = { GestureRange:new({ ges = "swipe", range = range }) },
-            Hold = { GestureRange:new({ ges = "hold", range = range }) },
-            HoldPan = { GestureRange:new({ ges = "hold_pan", range = range }) },
-            HoldRelease = { GestureRange:new({ ges = "hold_release", range = range }) },
-            Pan = { GestureRange:new({ ges = "pan", range = range }) },
-            PanRelease = { GestureRange:new({ ges = "pan_release", range = range }) },
-        }
-    end
 
     if not self._enabled then
         return
@@ -97,8 +67,6 @@ function ReadingRuler:addToMainMenu(menu_items)
     }
 end
 
-function ReadingRuler:update(bb, x, y) end
-
 function ReadingRuler:paintTo(bb, x, y)
     if not self._enabled then
         return
@@ -109,59 +77,10 @@ function ReadingRuler:paintTo(bb, x, y)
     InputContainer.paintTo(self, bb, x, y)
 end
 
-function ReadingRuler:onTouch(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onTouch")
-        return self._movable:onMovableTouch(arg, ges)
-    end
-end
-
-function ReadingRuler:onSwipe(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onSwipe")
-        return self._movable:onMovableSwipe(arg, ges)
-    end
-end
-
-function ReadingRuler:onHold(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onHold")
-        return self._movable:onMovableHold(arg, ges)
-    end
-end
-
-function ReadingRuler:onHoldPan(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onHoldPan")
-        return self._movable:onMovableHoldPan(arg, ges)
-    end
-end
-
-function ReadingRuler:onHoldRelease(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onHoldRelease")
-        return self._movable:onMovableHoldRelease(arg, ges)
-    end
-end
-
-function ReadingRuler:onPan(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onPan")
-        return self._movable:onMovablePan(arg, ges)
-    end
-end
-
-function ReadingRuler:onPanRelease(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onPanRelease")
-        return self._movable:onMovablePanRelease(arg, ges)
-    end
-end
-
 function ReadingRuler:buildUI()
     local screen_size = Screen:getSize()
 
-    local width = screen_size.w * 0.9
+    local width = screen_size.w
 
     -- Create the horizontal line widget
     local line_wget = LineWidget:new({
@@ -176,7 +95,6 @@ function ReadingRuler:buildUI()
         FrameContainer:new({
             color = Blitbuffer.COLOR_BLACK,
             bordersize = 0,
-            -- Add the group containing both line and overlay
             VerticalGroup:new({
                 line_wget,
             }),
@@ -191,10 +109,6 @@ end
 
 function ReadingRuler:resetPosition()
     self._movable:setMovedOffset({ x = 0, y = 0 })
-end
-
-function ReadingRuler:shouldHandleGesture(ges)
-    return ges.pos:intersectWith(self._movable.dimen)
 end
 
 function ReadingRuler:truncateHorizontalMovement()
