ee154e1221874506fc54ee4215ec92e526f9c389
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2025-04-28 03:37:33 +0700

    add caching and code organization

--- main.lua
+++ main.lua
@@ -22,6 +22,9 @@ local ReadingRuler = InputContainer:extend({
     _line_thickness = 5,
     _movable = nil,
 
+    _cached_texts = nil,
+    _cached_texts_page = nil,
+
     _ignore_events = {
         -- handle these events ourselves (to call the movablecontainer)
         "hold",
@@ -83,100 +86,44 @@ function ReadingRuler:addToMainMenu(menu_items)
     }
 end
 
-function ReadingRuler:onDispatcherRegisterActions()
-    Dispatcher:registerAction("reading_ruler_set_state", {
-        category = "string",
-        event = "ReadingRulerSetState",
-        title = _("Reading Ruler"),
-        general = true,
-        args = { true, false },
-        toggle = { _("enable"), _("disable") },
-    })
-
-    Dispatcher:registerAction("reading_ruler_toggle", {
-        category = "none",
-        event = "ReadingRulerToggle",
-        title = _("Reading Ruler: toggle"),
-        general = true,
-    })
-
-    Dispatcher:registerAction("reading_ruler_reset_position_action", {
-        category = "none",
-        event = "ReadingRulerResetPosition",
-        title = _("Reading Ruler: Reset position"),
-        general = true,
-    })
-end
-
 function ReadingRuler:paintTo(bb, x, y)
     if not self._enabled then
         return
     end
 
-    self:truncateHorizontalMovement()
-
     InputContainer.paintTo(self, bb, x, y)
 end
 
-function ReadingRuler:onSwipe(arg, ges)
-    if self:shouldHandleGesture(ges) then
-        logger.info("ReadingRuler:onSwipe")
-        return self._movable:onMovableSwipe(arg, ges)
-    end
+function ReadingRuler:onSwipe(_, ges)
+    local positions = self:getNearestTextPositions()
 
     if ges.direction == "south" then
-        local positions = self:getNearestTextPositions()
         if positions.next then
             logger.info("ReadingRuler: move down")
             self:move(0, positions.next.y + positions.next.h)
             UIManager:setDirty(self.view.dialog, "partial")
+            return true
         else
             logger.info("ReadingRuler: end of page")
         end
-
-        return true
     end
 
     if ges.direction == "north" then
-        local positions = self:getNearestTextPositions()
         if positions.prev then
             logger.info("ReadingRuler: move up")
             self:move(0, positions.prev.y + positions.prev.h)
             UIManager:setDirty(self.view.dialog, "partial")
+            return true
         else
             logger.info("ReadingRuler: start of page")
         end
-
-        return true
     end
-end
-
-function ReadingRuler:buildUI()
-    local screen_size = Screen:getSize()
-
-    self.dimen = Geom:new({ x = 0, y = 0, w = screen_size.w, h = screen_size.h })
-
-    local width = screen_size.w
-
-    -- Create the horizontal line widget
-    local line_wget = LineWidget:new({
-        background = Blitbuffer.gray(self._line_color_intensity),
-        dimen = Geom:new({ h = self._line_thickness, w = width }),
-    })
-
-    self._movable = MovableContainer:new({
-        ignore_events = self._ignore_events,
-        VerticalGroup:new({
-            line_wget,
-        }),
-    })
-
-    self[1] = self._movable
 end
 
 function ReadingRuler:onPageUpdate()
     local initial_y = 0
-    local texts = self:getTexts()
+    local texts = self:getTexts(true)
+
     if #texts.sboxes > 0 then
         initial_y = texts.sboxes[1].y + texts.sboxes[1].h
     end
@@ -217,27 +164,60 @@ function ReadingRuler:onReadingRulerToggle()
     UIManager:setDirty(self.view.dialog, "partial")
 end
 
-function ReadingRuler:truncateHorizontalMovement()
-    if self._movable and self._movable.dimen then
-        local offset = self._movable:getMovedOffset()
-        offset.x = 0
-        self._movable:setMovedOffset(offset)
-    end
+-- Custom fns
+function ReadingRuler:onDispatcherRegisterActions()
+    Dispatcher:registerAction("reading_ruler_set_state", {
+        category = "string",
+        event = "ReadingRulerSetState",
+        title = _("Reading Ruler"),
+        general = true,
+        args = { true, false },
+        toggle = { _("enable"), _("disable") },
+    })
+
+    Dispatcher:registerAction("reading_ruler_toggle", {
+        category = "none",
+        event = "ReadingRulerToggle",
+        title = _("Reading Ruler: toggle"),
+        general = true,
+    })
+
+    Dispatcher:registerAction("reading_ruler_reset_position_action", {
+        category = "none",
+        event = "ReadingRulerResetPosition",
+        title = _("Reading Ruler: Reset position"),
+        general = true,
+    })
+end
+
+function ReadingRuler:buildUI()
+    local screen_size = Screen:getSize()
+
+    self.dimen = Geom:new({ x = 0, y = 0, w = screen_size.w, h = screen_size.h })
+
+    local width = screen_size.w
+
+    -- Create the horizontal line widget
+    local line_wget = LineWidget:new({
+        background = Blitbuffer.gray(self._line_color_intensity),
+        dimen = Geom:new({ h = self._line_thickness, w = width }),
+    })
+
+    self._movable = MovableContainer:new({
+        ignore_events = self._ignore_events,
+        VerticalGroup:new({
+            line_wget,
+        }),
+    })
+
+    self[1] = self._movable
 end
 
 function ReadingRuler:move(x, y)
-    -- logger.info("ReadingRuler: Move to ", x, y)
     if not self._enabled then
         self._enabled = true
         UIManager:setDirty(self.view.dialog, "partial")
     end
-
-    local nearest_text = self:getNearestTextPositions(y).curr
-    if nearest_text and y < nearest_text.y + nearest_text.h then
-        y = nearest_text.y + nearest_text.h
-    end
-
-    -- logger.info("ReadingRuler: Move to (snapped)", x, y)
 
     local offset = self._movable:getMovedOffset()
     offset.x = x
@@ -246,17 +226,26 @@ function ReadingRuler:move(x, y)
     self._movable:setMovedOffset(offset)
 end
 
-function ReadingRuler:shouldHandleGesture(ges)
-    return ges.pos:intersectWith(self._movable.dimen)
-end
-
-function ReadingRuler:getTexts()
+function ReadingRuler:getTexts(ignore_cache)
     local page = self.document:getCurrentPage()
-    return self.ui.document:getTextFromPositions(
+
+    if not ignore_cache and self._cached_texts and self._cached_texts_page == page then
+        logger.info("ReadingRuler: cache hit")
+        return self._cached_texts
+    end
+
+    logger.info("ReadingRuler: cache miss")
+
+    local texts = self.ui.document:getTextFromPositions(
         { x = 0, y = 0, page = page },
         { x = Screen:getWidth(), y = Screen:getHeight() },
         true
     )
+
+    self._cached_texts = texts
+    self._cached_texts_page = page
+
+    return texts
 end
 
 function ReadingRuler:getNearestTextPositions(y)
